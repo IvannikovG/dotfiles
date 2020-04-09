107e6a12424b5705f7651cbd452bb9e4bcece31e
mput <putintsev@gmail.com>
2020-04-09 12:33:58 +0300

    Conf updates.

--- link/spacemacs.d/init.el
+++ link/spacemacs.d/init.el
@@ -85,7 +85,7 @@ This function should only modify configuration layer settings."
      sql
      restclient
      theming
-     tmux
+     ;; tmux
      )
 
    ;; List of additional packages that will be installed without being
